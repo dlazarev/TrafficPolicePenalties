8edec112c78a49bca64e286b2e33d0430e2e9d60
Dmitry Lazarev <diman.lazarev@gmail.com>
2012-12-15 17:03:17 +0400

    class BTree added

---
+++ TrafficPolicePenalties/BTree.cpp
@@ -0,0 +1,9 @@
+//
+//  BTree.cpp
+//  TrafficPolicePenalties
+//
+//  Created by Dmitry Lazarev on 15.12.12.
+//  Copyright (c) 2012 Ilya Lazarev. All rights reserved.
+//
+
+#include "BTree.h"
---
+++ TrafficPolicePenalties/BTree.h
@@ -0,0 +1,21 @@
+//
+//  BTree.h
+//  TrafficPolicePenalties
+//
+//  Created by Dmitry Lazarev on 15.12.12.
+//  Copyright (c) 2012 Ilya Lazarev. All rights reserved.
+//
+
+#ifndef __TrafficPolicePenalties__BTree__
+#define __TrafficPolicePenalties__BTree__
+
+#include <iostream>
+#include "service.h"
+
+class BTree {
+private:
+    Node *root;
+    
+};
+
+#endif /* defined(__TrafficPolicePenalties__BTree__) */
