62d12c18ff0f6c6bee07af69b36065bc6eebe9ce
Dmitry Lazarev <diman.lazarev@gmail.com>
2012-12-15 16:13:48 +0400

    Initial Commit

---
+++ TrafficPolicePenalties/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TrafficPolicePenalties
+//
+//  Created by Dmitry Lazarev on 15.12.12.
+//  Copyright (c) 2012 Ilya Lazarev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
