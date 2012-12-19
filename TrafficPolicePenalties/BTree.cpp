3f1c6ca7bca2c70a6c8901ab4adc6bc07c6df09e
Илья Лазарев <ramporez@MacBook-Pro-Ila.local>
2012-12-19 20:19:23 +0400

    First commit on  the new macbook pro

--- TrafficPolicePenalties/BTree.cpp
+++ TrafficPolicePenalties/BTree.cpp
@@ -1,10 +1,7 @@
 //
 //  BTree.cpp
 //  TrafficPolicePenalties
-//
-//  Created by Dmitry Lazarev on 15.12.12.
-//  Copyright (c) 2012 Ilya Lazarev. All rights reserved.
-//
+// Goodbye win32
 
 #include "BTree.h"
 #include "service.h"
